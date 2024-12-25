33c002b588489c82bfd39ea6d38a92db762b9dbc
Cyrus <cyhfung@gmail.com>
2024-12-25 02:51:54 -0500

    reorganization + initial todo list

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/cfung89/messaging/backend
 
 go 1.23.0
+
+require github.com/stretchr/testify v1.10.0 // indirect
---
+++ backend/pkg/auth/auth.go
@@ -0,0 +1,12 @@
+package authentication
+
+import (
+	"fmt"
+)
+
+type AuthServer struct {
+}
+
+func test() {
+	fmt.Println("Hello, World")
+}
